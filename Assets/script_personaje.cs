7719321a7d5d39fb29eb4583f92f91a5b84e10a7
luisangel1209 <56208306+luisangel1209@users.noreply.github.com>
2020-10-25 07:36:08 -0600

    Error
    Error Video 4 - No corre

--- Assets/script_personaje.cs
+++ Assets/script_personaje.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class script_personaje : MonoBehaviour
 {
     Animator anim;
+    Rigidbody2D rb;
+    public float fuerzaSalto;
+    public bool enPiso;
+    public Transform refPie;
+    public float velX = 70f;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump")) anim.SetTrigger("Rascar");
+        float movX;
+        movX = Input.GetAxis("Horizontal");
+        anim.SetFloat("absMovX", Mathf.Abs(movX));
+        rb.velocity = new Vector2(velX * movX, rb.velocity.y);
+
+        enPiso = Physics2D.OverlapCircle(refPie.position,1f,1<<8);
+        anim.SetBool("enPiso", enPiso);
+
+        if (Input.GetButtonDown("Jump") && enPiso){
+            rb.AddForce(new Vector2(0, fuerzaSalto), ForceMode2D.Impulse);
+        }
     }
 }
