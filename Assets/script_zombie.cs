55c18b70f531dc12472d6280bc3409fc31fc97b6
MiguelAngelLopezH <36934468+MiguelAngelLopezH@users.noreply.github.com>
2020-11-16 13:46:20 -0600

    mas zombies
    circleradius

--- Assets/script_zombie.cs
+++ Assets/script_zombie.cs
@@ -13,6 +13,8 @@ public class script_zombie : MonoBehaviour
 
     public float velCaminata = 10f;
     int direccion = 1;
+    Vector3 escalaOriginal;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,8 @@ public class script_zombie : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         limiteCaminataDer = transform.position.x + GetComponent<CircleCollider2D>().radius;
         limiteCaminataIzq = transform.position.x - GetComponent<CircleCollider2D>().radius;
+
+        escalaOriginal = transform.localScale;
     }
 
     // Update is called once per frame
@@ -28,6 +32,6 @@ public class script_zombie : MonoBehaviour
         rb.velocity = new Vector2(velCaminata * direccion, rb.velocity.y);
         if (transform.position.x < limiteCaminataIzq) direccion = 1;
         if (transform.position.x > limiteCaminataDer) direccion = -1;
-        transform.localScale = new Vector3(1.03496f * direccion, 1.03496f, 1.03496f);
+        transform.localScale = new Vector3(escalaOriginal.x * direccion, escalaOriginal.y, escalaOriginal.z);
     }
 }
