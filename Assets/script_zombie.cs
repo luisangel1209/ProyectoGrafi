f2c3830ad14e2df627f5b8289c3c5c147e985f3a
MiguelAngelLopezH <36934468+MiguelAngelLopezH@users.noreply.github.com>
2020-11-15 21:45:30 -0600

    circleCollider
    cicleCollider

--- Assets/script_zombie.cs
+++ Assets/script_zombie.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class script_zombie : MonoBehaviour
 {
 
+    Animator anim;
     Rigidbody2D rb;
+    float limiteCaminataIzq;
+    float limiteCaminataDer;
+
+    public float velCaminata = 10f;
+    int direccion = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        limiteCaminataDer = transform.position.x + GetComponent<CircleCollider2D>().radius;
+        limiteCaminataIzq = transform.position.x - GetComponent<CircleCollider2D>().radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(40.rb.velocity.y);
+        rb.velocity = new Vector2(velCaminata * direccion, rb.velocity.y);
+        if (transform.position.x < limiteCaminataIzq) direccion = 1;
+        if (transform.position.x > limiteCaminataDer) direccion = -1;
+        transform.localScale = new Vector3(1.03496f * direccion, 1.03496f, 1.03496f);
     }
 }
